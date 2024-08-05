be7e9d66fe3a563cbc5e9295134f9758c8cb869c
William (SD) <william@speeddigital.com>
2024-08-05 08:25:53 -0500

    Added some edits to app.js file

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 // Whitelist of allowed origins
-const whitelist = [];
+const whitelist = [''];
 
 // Custom CORS configuration
 const corsOptions = {
